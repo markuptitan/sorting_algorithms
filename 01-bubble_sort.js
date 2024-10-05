cac95e58ed917957a62341d2f4302db5960c7ed5
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-10-05 16:06:58 +0200

    Use destructuring instead of a temp value

--- 01-bubble_sort.js
+++ 01-bubble_sort.js
@@ -6,9 +6,10 @@ const bubbleSort = (array) => {
     for (let innerIndex = 0; innerIndex < arrayLength; innerIndex++) {
       if (array[innerIndex] > array[innerIndex + 1]) {
         // Swap elements
-        const temp = array[innerIndex];
-        array[innerIndex] = array[innerIndex + 1];
-        array[innerIndex + 1] = temp;
+        [array[innerIndex], array[innerIndex + 1]] = [
+          array[innerIndex + 1],
+          array[innerIndex],
+        ];
         swapped = true;
       }
     }
